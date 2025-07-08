0a6f24b3436cd316982856686720527cb392e645
Svist227 <danilovroma466@gmail.com>
2025-07-08 15:52:50 +0300

    task1

--- Practice/Practice.cpp
+++ Practice/Practice.cpp
@@ -8,6 +8,7 @@ int main()
     
     cout << A / B << endl << B / A << endl << A % B << endl << B % A;
     cout << endl << float(A) / float(B) << endl << B / float(A);
-    return 0;
+    return 0;                 
+
 }
 

547484b4954bf674b79d4a010a8065507c6a44a9
Svist227 <danilovroma466@gmail.com>
2025-07-09 10:34:56 +0300

    Rename files

---
+++ Practice/task1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int A = 11, B = 6;
+
+    cout << A / B << endl << B / A << endl << A % B << endl << B % A;
+    cout << endl << float(A) / float(B) << endl << B / float(A);
+    return 0;
+
+}
+
